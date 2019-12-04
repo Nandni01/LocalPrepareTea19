ade36f9cde8a398e6f0725c6f639148e79f73812
Neetu <Neetu@DESKTOP-Q8IS7BV>
2019-12-04 14:57:50 +0530

    Initial commite Project

---
+++ PrepareTea/src/com/MainTea.java
@@ -0,0 +1,10 @@
+package com;
+
+public class MainTea {
+	public static void main(String[] args) {
+		System.out.println("im in main");
+		Tea t=new Tea();
+		t.prepare();
+	}
+
+}
---
+++ PrepareTea/src/com/Tea.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Tea {
+	
+	public void prepare()
+	{
+		
+		System.out.println("i m in Tea");
+	}
+
+}
